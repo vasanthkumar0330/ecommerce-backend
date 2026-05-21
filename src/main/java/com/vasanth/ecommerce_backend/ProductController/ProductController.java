7fcb2b761e9583a6fced3f1a99fe1761e9c90c37
vasanthkumar-S0330 <saravedivasanth@gmail.com>
2026-05-22 00:54:04 +0530

    backend project

--- src/main/java/com/vasanth/ecommerce_backend/ProductController/ProductController.java
+++ src/main/java/com/vasanth/ecommerce_backend/ProductController/ProductController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/products")
 public class ProductController {
